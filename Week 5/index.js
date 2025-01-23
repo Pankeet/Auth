94b9aeb2bf6da67be1630080ff74b106b0c5419a
Pankeet <pankeet04@gmail.com>
2025-01-24 00:28:03 +0530

    Delete Week 5 directory

--- Week 5/index.js
+++
@@ -1,9 +0,0 @@
-const express = require ('express');
-const app = express();
-
-app.get('/sign', function(req, res){
-        res.sendFile(__dirname + "/Week 5/Zero1.html")
-});
-
-
-app.listen(8080);
