00d3c722defa5c6c7050b204fa6b3d00c329ad26
Pankeet <pankeet04@gmail.com>
2025-01-24 00:28:48 +0530

    Delete Week3 directory

--- Week3/Week 3f/script.js
+++
@@ -1,8 +0,0 @@
-var c = 0;
-function incrementaftertime()
-{
-    document.querySelector("h4").innerHTML = c;
-    document.querySelectorAll("h4")[1].innerHTML = c;
-    c = c + 1;
-}
-setInterval(incrementaftertime,1000);
