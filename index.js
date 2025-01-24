c65606c512b7bff943c66464893a34646e498e18
Pankeet <pankeet04@gmail.com>
2025-01-24 18:00:05 +0530

    Update Version 1.3

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require("express");
 const jwt = require('jsonwebtoken');
 const app = express();
-const JWT_SECRET = "Pankeetisgreat";
-// Local Database
-const user = [];
+const JWT_SECRET = "AuthUSingJwt";
+// Currently using Local Database
+let user = [];
 
 app.use(express.json());
 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/public/index.html");
+})
+// Middleware for authentication
+function auth(req, res, next){
+        const token = req.headers.token;
+        const decodedtoken = jwt.verify(token, JWT_SECRET);
+        if(decodedtoken.username){  
+                req.username = decodedtoken.username;
+                console.log("verified user "+decodedtoken.username);
+                next();
+        }
+        else{
+            res.send("You are not logged in ! Please log in again");
+        }
+}
+
+function logger(req,res, next){
+    console.log(req.method +" req came!");
+    next();
+}
 // SIGNUP
-app.post('/signup', function(req,res){
+app.post('/signup', logger, function(req,res){
         const username = req.body.username;
         const password = req.body.password;
+        // Check if the user already exsists
         let b = true ;
         for(let i=0;i<user.length;i++){
             if(user[i].username == username){
@@ -23,66 +45,54 @@ app.post('/signup', function(req,res){
             username : username,
             password : password
         })
+        console.log(user)
     }
         res.send("You have signed up! 🍾");
 })
 
 // SIGNIN
-app.post("/signin", function(req, res){
+app.post("/signin", logger , function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
-
+    const password =  req.body.password;
     let foundi = null;
-
-    for(let i=0;i<user.length;i++){
-        if(user[i].username == username && user[i].password == password){
-           
+    for(let i=0;i<user.length;i++){  
+        if (user[i].username.trim() === username.trim() && user[i].password.trim() === password.trim()) {
             foundi = user[i];
+            break;
         }
         }
+
         if(foundi){
         const token = jwt.sign({
             username : foundi.username
         }, JWT_SECRET);
 
         foundi.token = token;
+        console.log(token)
 
         res.json({
             token : token
             })
-        }
+        } 
         else{
-            res.status(403).send({
-                message: "Invalid Token"
-            })
+            res.status(403).send("Invalid Token");
     }
-    console.log(user);
+  
 })
 
 // ME endpoint
-app.get('/me', function (req, res) {
-    const token = req.headers.token;
+app.get('/me', logger, auth ,function (req, res) {
 
-    try {
-        // Verify token
-        const foundme = jwt.verify(token, JWT_SECRET);
-        const usern = foundme.username;
-
-        // Find the user
         for (let i = 0; i < user.length; i++) {
-            if (user[i].username === usern) {
+            if (user[i].username === req.username) {
                 return res.json({
-                    Username: usern
+                    Username : req.username
                 });
             }
         }
 
         // If no user is found
         res.status(401).send("Invalid Token");
-    } catch (error) {
-        // Handle invalid token or verification errors
-        res.status(401).send("Invalid Token");
-    }
-});
-
-app.listen(3000);  
+    });
+ 
+app.listen(8080);   
